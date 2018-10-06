bddd6cf9f2375ed778ce9b96b2b53db38e9cf1e9
CIS5517 <tuc48831@temple.edu>
2018-10-06 13:43:23 -0400

    First upload of high level overview in comments

--- Assignment1/src/hadoopExample/WordCount.java
+++ Assignment1/src/hadoopExample/WordCount.java
@@ -1,5 +1,10 @@
 package hadoopExample;
 
-public class WordCount {
-
+public class WordCount{
+	//map file as key to the values of the words and count inside the segments of the file
+	
+	//then we need to group by the keysm and words those key value pairs to the appropriate nodes
+	
+	//reduce would be take all of the various key value pairs and add up all the counts
+	
 }
