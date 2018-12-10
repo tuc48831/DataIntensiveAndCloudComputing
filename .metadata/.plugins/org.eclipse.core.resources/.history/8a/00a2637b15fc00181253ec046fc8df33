3ba2f9263600dd992c8dd191ad3d2ba08b923531
CIS5517 <tuc48831@temple.edu>
2018-12-09 23:05:48 -0500

    upload of partially complete spark work

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/8a/00a2637b15fc00181253ec046fc8df33
@@ -0,0 +1,8 @@
+'''
+Created on Dec 5, 2018
+
+@author: cis5517
+'''
+
+if __name__ == '__main__':
+    pass
--- sparkImpl/Main/testSpark.py
+++ sparkImpl/Main/testSpark.py
@@ -4,6 +4,7 @@ import re
 import datetime
 import pyspark.sql as sq
 from __builtin__ import file
+from pyspark.sql.functions import input_file_name
 
 #call with /usr/local/spark/bin/spark-submit ~/git/DataIntensiveAndCloudComputing/sparkImpl/Main/testSpark.py 
 
@@ -16,23 +17,22 @@ spark = sq.SparkSession.builder.appName("merge_jsons").getOrCreate()
 if __name__ == '__main__':
     path = "/home/cis5517/Documents/inputAndOutput/input/"
     files = os.listdir(path)
-    jsons = []
-    for jsonFile in files:
-        strings = re.split("_|\.", jsonFile)
-        #get the file name, split it to the ID and timestamp
-        articleID = strings[0]
-        timeStamp = strings[1]
-        print("timestamp is:" +timeStamp)
-        #decode the timestamp into a real date so i can work with it
-        newDateTime = datetime.datetime.strptime(timeStamp, "%Y%m%d%H%M%S")
-        
-        fullpath = path + jsonFile
-        data = spark.read.json(fullpath)
-        data.update("scrapeTimestamp", str(newDateTime))
-        jsons.append(data)
+    data = spark.read.json(path)
     
-    #convert to dataframes   
-    for item in jsons:
-        df = spark.createDataFrame(item)
     
-    print 'output!: ', str(union)
+    data = data.withColumn('filePath', input_file_name())
+    
+    data.show()
+    #get the fileName column and expand it out into article ID
+    #example filename file:///home/cis5517/Documents/inputAndOutput/input/29654_20181116114436.json
+    split_col = sq.functions.split(data['filePath'], '/input/')
+    data = data.withColumn('fileName', split_col.getItem(1))
+    
+    split_col = sq.functions.split(data['fileName'], '_')
+    data = data.withColumn('articleId', split_col.getItem(0))
+    #then merge on articleID
+    data.show()
+    #for each article ID
+    #newData = data.groupby('articleId').agg(sq.functions.collect_set('response')).collect()
+    
+    
